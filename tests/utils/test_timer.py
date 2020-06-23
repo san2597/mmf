98432175595a7fc787697458b08bc0f37d470015
Vedanuj Goswami <vedanujg@gmail.com>
2020-06-22 20:50:54 -0700

    [try tests for windows]

--- tests/utils/test_timer.py
+++ tests/utils/test_timer.py
@@ -8,7 +8,7 @@ from mmf.utils.timer import Timer
 class TestUtilsTimer(unittest.TestCase):
     def test_get_current(self):
         timer = Timer()
-        expected = "000ms"
+        expected = "0ms"
 
         self.assertEqual(timer.get_current(), expected)
 
@@ -16,13 +16,13 @@ class TestUtilsTimer(unittest.TestCase):
         timer = Timer()
         time.sleep(2)
         timer.reset()
-        expected = "000ms"
+        expected = "0ms"
 
         self.assertEqual(timer.get_current(), expected)
 
     def test_get_time_since_start(self):
         timer = Timer()
         time.sleep(2)
-        expected = "02s "
+        expected = "2s"
 
         self.assertTrue(expected in timer.get_time_since_start())
